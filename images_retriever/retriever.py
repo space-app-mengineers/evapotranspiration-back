87c50bc3df2150a19feb00bacf23b7b6c5d1bc82
Juan Pablo Herrera <juherrera@ug.uchile.cl>
2024-10-06 22:46:01 -0300

    et

--- images_retriever/retriever.py
+++ images_retriever/retriever.py
@@ -41,16 +41,21 @@ def get_ee_image(lat: float, lon: float) -> bytes:
 
 def get_polygon_image(x1: float, y1: float, x2: float, y2: float, x3: float, y3: float, x4: float, y4: float) -> bytes:
 
+    print("hola")
     x_c = (x1 + x2 + x3 + x4) / 4
     y_c = (y1 + y2 + y3 + y4) / 4
     roi_center = [x_c, y_c]
 
     # Define a bounding box (coordinates: [min_lon, min_lat, max_lon, max_lat])
-    polygon = ee.Geometry.Polygon([[x1, y1], [x2, y2], [x3, y3], [x4, y4]])
+    polygon = ee.Geometry.Polygon([[-70.6483, -33.4569],
+        [-70.6483, -33.4469],
+        [-70.6383, -33.4469],
+        [-70.6383, -33.4569],
+        [-70.6483, -33.4569]])
 
     landsat = ee.ImageCollection('LANDSAT/LC08/C02/T1_TOA') \
-            .filterBounds(polygon) \
             .filterDate('2023-01-01', '2023-12-31') \
+            .filterBounds(polygon) \
             # .sort('CLOUD_COVER')
 
     # Select the least cloudy image
@@ -60,13 +65,13 @@ def get_polygon_image(x1: float, y1: float, x2: float, y2: float, x3: float, y3:
 
     region = polygon.buffer(100).bounds().getInfo()
 
+    print(image.getInfo())
     url = image.getThumbUrl({
-        'region': region['coordinates'],
+        'region': polygon,
         'dimensions': 512,
         'format': 'png',
         'min': 0,
-        'max': 0.3,
-        'format': 'png'
+        'max': 0.3
     })
 
     response = requests.get(url)
@@ -80,3 +85,45 @@ def get_polygon_image(x1: float, y1: float, x2: float, y2: float, x3: float, y3:
 
     return response.content
 
+def get_asd():
+    import ee
+    import geeet
+    import geemap
+    import geemap.colormaps as cm
+    from geeet.eepredefined import landsat
+
+    region = dict(type="Point", coordinates=[-70.646652, -33.605887])  # Santiago, Chile
+
+    # Define a custom workflow (TSEB model + LE extrapolation)
+    workflow = [
+        geeet.tseb.tseb_series,
+        landsat.extrapolate_LE    # this adds the "ET" band, in mm/day
+    ]
+
+    landsat_era5_tseb_collection = landsat.mapped_collection(
+        workflow,
+        date_start = "2024-04-01",
+        date_end = "2024-08-01",
+        region = region, 
+        era5 = True,
+        timeZone="America/Santiago" # the time property will be set in local time (UTC -3 for this example)
+    )
+    # Select one image by LANDSAT_INDEX:
+    image = (
+        landsat_era5_tseb_collection
+        .filter(ee.Filter.eq("LANDSAT_INDEX", 
+        landsat_era5_tseb_collection.aggregate_array("LANDSAT_INDEX").getInfo()[0]
+        ))
+        .first()
+        .clip(
+            ee.Geometry(region)
+            .buffer(1000)   # area around the point in meters
+            .bounds()        
+        )
+    )
+
+
+    et = image.select('ET')
+    filename="et_image.png"
+    geemap.ee_export_image(et, filename="et_image.png", scale=30, region=ee.Geometry(region).buffer(1000).bounds(), file_per_band=False)
+    return filename
--- main.py
+++ main.py
@@ -26,6 +26,20 @@ async def get_image(lat: float, lon: float):
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"An error occurred: {str(e)}")
 
+@app.get("/get-asd/")
+async def get_asd():
+    try:
+        image_data = retriever.get_asd()
+        with tempfile.NamedTemporaryFile(delete=False, suffix=".png") as temp_file:
+            # temp_file.write(image_data)
+            temp_file_path = temp_file.name
+
+        # Return the image file
+        return FileResponse(temp_file_path, media_type="image/tiff", filename=image_data)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"An error occurred: {str(e)}")
+    
+
 @app.get("/get-polygon-image/{x1}/{y1}/{x2}/{y2}/{x3}/{y3}/{x4}/{y4}")
 async def get_polygon_image(
             x1: float, y1: float,
